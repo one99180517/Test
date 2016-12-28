b14c2703a82d30872472227457a38d6a05d361ee
Xin-Yuan Su <one99180517@hotmail.com>
2016-12-29 02:05:32 +0800

    rollback to tag : ori

--- hello.c
+++ hello.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-#include <string.h>
+// #include <string.h>
 
 int main(void)
 {
     printf("hello world!\n");
     return 0;
-}
-
-void test (void) 
-{
-    printf("test\n");
 }
